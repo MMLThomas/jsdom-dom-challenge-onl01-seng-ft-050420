3731b2370641574b046b05f76f97d2962f8bd5f8
michael thomas <michael.thomas2014@gmail.com>
2020-08-18 06:51:39 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -74,6 +74,18 @@ document.querySelector('#pause').addEventListener('click', function(event){
 document.querySelector('')
 
 
+document.querySelector('#create-task-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let node = document.createElement("Li");
+    let task = document.getElementById('new-task-description').value;
+    let new_node = document.createTextNode(task);
+    node.appendChild(new_node);
+    let taskList = document.getElementById('tasks');
+    taskList.appendChild(node);
+    
+  });
+
 
 
 
