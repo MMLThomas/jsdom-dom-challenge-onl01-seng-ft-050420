d084386e120749deca53a1f2ac55ec1dab0e24d9
michael thomas <michael.thomas2014@gmail.com>
2020-08-18 07:06:54 +0000

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,88 @@
+let time = 0;
+
+let count = setInterval(add_second, 1000);
+
+const counter = document.querySelector('#counter');
+
+let num_likes = 0;
+
+
+function add_second(){
+  counter.innerText = time;
+  time += 1;
+}
+
+function minus_second(){
+  counter.innerText = time;
+  time -= 1;
+}
+
+function like(){
+  let count = counter.innerText
+  
+  if( document.querySelector(`#liked-${count}`)){
+    num_likes += 1
+    document.querySelector(`#liked-${count}`).innerText = `${count} has been liked ${num_likes} times`;
+  }
+  else{
+    num_likes = 1
+    let node = document.createElement("Li");
+    node.setAttribute("id", `liked-${count}`)
+    let newContent = document.createTextNode(`${count} has been liked 1 time`);
+    node.appendChild(newContent)
+    document.querySelector('.likes').appendChild(node)
+  }
+}
+
+
+function pause(){
+  let pause = document.querySelector('#pause')
+  let minus = document.querySelector('#minus');
+  let plus = document.querySelector('#plus');
+  let heart = document.querySelector('#heart');
+  let submit = document.querySelector('#submit');
+  minus.disabled = !minus.disabled;
+  plus.disabled = !plus.disabled;
+  heart.disabled = !heart.disabled;
+  submit.disabled = !submit.disabled;
+  if (pause.innerText == 'pause'){
+     let stop_count = clearInterval(count);
+    pause.innerText = 'resume'
+  }
+  else{
+    count = setInterval(add_second, 1000);
+    pause.innerText = 'pause';
+  }
+}
+
+document.querySelector('#plus').addEventListener('click', function(event){
+  add_second()
+});
+
+document.querySelector('#minus').addEventListener('click', function(event){
+  minus_second()
+});
+  
+document.querySelector('#heart').addEventListener('click', function(event){
+  like()
+});
+
+document.querySelector('#pause').addEventListener('click', function(event){
+  pause()
+});
+
+
+document.querySelector('#comment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    let node = document.createElement("Li");
+    let comment = document.querySelector('#comment-input').value;
+    let new_node = document.createTextNode(comment);
+    node.appendChild(new_node);
+    let comments = document.querySelector('#list');
+    comments.appendChild(node);
+  });
+
+
+
+
