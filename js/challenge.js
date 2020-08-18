cd9ed410edade230729e2730eba74cecf69cd9ae
michael thomas <michael.thomas2014@gmail.com>
2020-08-18 06:48:51 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -50,7 +50,7 @@ function pause(){
     pause.innerText = 'resume'
   }
   else{
-    let count = setInterval(add_second, 1000);
+    count = setInterval(add_second, 1000);
     pause.innerText = 'pause';
   }
 }
